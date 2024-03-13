d528294034663abf017fae122f074b7752326a32
Regina <reginafloretta@gmail.com>
2024-03-13 10:33:04 +0800

    commit changes

--- 1_output.py
+++ 1_output.py
@@ -4,8 +4,11 @@
 #
 
 # 1. Input 
+x1 = int(input('Type x1:')) #or else it might be specified as other types (such as string)
+x2 = int(input('Type x2:'))
 
 # 2. Process
+sum = x1+x2
 
 # 3. Output
-print('Hello World!')
+print(f'Sum : {sum}')
